d2f54de219446e77dc88ddeb715659fcbfaad337
Leen Fani <118082430+leenfani@users.noreply.github.com>
2023-04-15 02:21:45 +0200

    Delete lg.js

--- lg.js
+++
@@ -1,6 +0,0 @@
-let loregbox = document.querySelector('.logreg-box');
-let signinlink = document.querySelector('.signin-link');
-let registerlink = document.querySelector('.register-link');
-let formsignup = document.querySelector('.form-signup');
-
-
