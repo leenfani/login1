16585b3f658891bedf0dad05f1d06752a21f698d
leenfani <leenbeg26.com>
2023-04-15 02:20:10 +0200

    first commit

---
+++ lg.js
@@ -0,0 +1,6 @@
+let loregbox = document.querySelector('.logreg-box');
+let signinlink = document.querySelector('.signin-link');
+let registerlink = document.querySelector('.register-link');
+let formsignup = document.querySelector('.form-signup');
+
+
